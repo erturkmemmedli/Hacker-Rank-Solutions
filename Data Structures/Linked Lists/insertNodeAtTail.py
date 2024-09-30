fcb49ddb581fa650def324b73def21dbcece0b8d
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2024-09-30 20:48:38 +0400

    Create insertNodeAtTail.py

---
+++ Data Structures/Linked Lists/insertNodeAtTail.py
@@ -0,0 +1,67 @@
+# Insert a Node at the Tail of a Linked List
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the insertNodeAtTail function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def insertNodeAtTail(head, data):
+    temp = head
+    node = SinglyLinkedListNode(data)
+    
+    while temp:
+        if temp.next:
+            temp = temp.next
+        else:
+            temp.next = node
+            break
+            
+    return head if head else node
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for i in range(llist_count):
+        llist_item = int(input())
+        llist_head = insertNodeAtTail(llist.head, llist_item)
+        llist.head = llist_head
+
+    print_singly_linked_list(llist.head, '\n', fptr)
+    fptr.write('\n')
+
+    fptr.close()
