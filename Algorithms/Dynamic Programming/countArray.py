ad02d871ca4cbaef1fb492b2e98400328a9ba765
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2024-10-01 11:15:54 +0400

    Update countArray.py

--- Algorithms/Dynamic Programming/countArray.py
+++ Algorithms/Dynamic Programming/countArray.py
@@ -32,6 +32,22 @@ def countArray(n, k, x):
         prev_x = tmp
         
     return prev_not_x
+
+
+def countArray_alternative(n, k, x):
+    # Return the number of ways to fill in the array.
+    mod = 1_000_000_007
+    
+    dp = [[None for _ in range(n)] for _ in range(2)]
+            
+    dp[0][0] = 1 if x == 1 else 0
+    dp[1][0] = 0 if x == 1 else 1
+
+    for i in range(1, n):
+        dp[0][i] = dp[1][i - 1] % mod
+        dp[1][i] = (dp[0][i - 1] * (k - 1) + dp[1][i - 1] * (k - 2)) % mod
+
+    return dp[0][n - 1]
     
     
 if __name__ == '__main__':
