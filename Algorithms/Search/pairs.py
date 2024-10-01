ca17691f58278f1ce6cc7202f80c440f8c5f21c8
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2024-10-01 11:08:59 +0400

    Create pairs.py

---
+++ Algorithms/Search/pairs.py
@@ -0,0 +1,47 @@
+# Pairs
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pairs' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY arr
+#
+
+def pairs(k, arr):
+    # Write your code here
+    arr = set(arr)
+    result = 0
+    
+    for num in arr:
+        if num - k in arr:
+            result += 1
+        
+    return result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = pairs(k, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
