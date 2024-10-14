590270257074c20957b6d90a20062cfc9ca8c09a
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2024-10-14 19:49:12 +0400

    Create queensAttack.java

---
+++ Algorithms/Implementation/queensAttack.java
@@ -0,0 +1,96 @@
+// Queen's Attack II
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'queensAttack' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER k
+     *  3. INTEGER r_q
+     *  4. INTEGER c_q
+     *  5. 2D_INTEGER_ARRAY obstacles
+     */
+
+    public static int queensAttack(int n, int k, int r_q, int c_q, List<List<Integer>> obstacles) {
+        // Write your code here
+        int[][] directions = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+        HashSet<String> set = new HashSet<>();
+        int attackable = 0;
+        
+        for (List<Integer> obstacle: obstacles) {
+            set.add(obstacle.get(0) + " " + obstacle.get(1));
+        }
+        
+        for (int[] dir: directions) {
+            int x = dir[0], y = dir[1];
+            int r = r_q, c = c_q;
+            
+            while (r + x >= 1 && r + x <= n && c + y >= 1 && c + y <= n && !set.contains((r + x) + " " + (c + y))) {
+                System.out.println((r + x) + " " + (r + y));
+                r += x;
+                c += y;
+                attackable++;
+            }
+        }
+        
+        return attackable;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        String[] secondMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int r_q = Integer.parseInt(secondMultipleInput[0]);
+
+        int c_q = Integer.parseInt(secondMultipleInput[1]);
+
+        List<List<Integer>> obstacles = new ArrayList<>();
+
+        IntStream.range(0, k).forEach(i -> {
+            try {
+                obstacles.add(
+                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int result = Result.queensAttack(n, k, r_q, c_q, obstacles);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
