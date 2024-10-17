07c4604667dbf20ebdb03a7058d9e8cb8b0beb15
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2024-10-18 01:42:48 +0400

    Create fairRations.py

---
+++ Algorithms/Implementation/fairRations.py
@@ -0,0 +1,42 @@
+# Fair Rations
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'fairRations' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts INTEGER_ARRAY B as parameter.
+#
+
+def fairRations(B):
+    # Write your code here
+    count = 0
+    
+    for i in range(len(B) - 1):
+        if B[i] % 2 == 1:
+            B[i + 1] += 1
+            count += 2
+    
+    return "NO" if B[-1] % 2 == 1 else str(count)
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    N = int(input().strip())
+
+    B = list(map(int, input().rstrip().split()))
+
+    result = fairRations(B)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
