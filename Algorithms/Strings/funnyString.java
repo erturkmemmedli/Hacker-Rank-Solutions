ec739fb3b6cbd1e91b287b7ebd67fae6b60729fa
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2024-10-22 00:45:55 +0400

    Create funnyString.java

---
+++ Algorithms/Strings/funnyString.java
@@ -0,0 +1,64 @@
+// Funny String
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'funnyString' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String funnyString(String s) {
+        // Write your code here
+        List<Integer> diff = new ArrayList<>();
+        
+        for (int i = 1; i < s.length(); i++) {
+            diff.add(Math.abs(s.charAt(i) - s.charAt(i - 1)));
+        }
+        
+        List<Integer> reversedDiff = new ArrayList<>(diff);
+        Collections.reverse(reversedDiff);
+        
+        return (diff.equals(reversedDiff)) ? "Funny" : "Not Funny";
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                String result = Result.funnyString(s);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
